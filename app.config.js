c60710dce07f9f605dc779190a8f2b573439730f
cjwarx12 <cristalqwert@gmail.com>
2026-03-18 21:31:17 -0600

    Config - app.config.js con API Key de Maps desde EAS Secrets

---
+++ app.config.js
@@ -0,0 +1,45 @@
+export default {
+  expo: {
+    name: "TuPupuseriaApp",
+    slug: "tupupuseriaapp",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#FDF6EE"
+    },
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      package: "com.cjwarx12.tupupuseriaapp",
+      versionCode: 1,
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_MAPS_API_KEY
+        }
+      },
+      adaptiveIcon: {
+        backgroundColor: "#FDF6EE",
+        foregroundImage: "./assets/android-icon-foreground.png",
+        backgroundImage: "./assets/android-icon-background.png",
+        monochromeImage: "./assets/android-icon-monochrome.png"
+      },
+      permissions: [
+        "ACCESS_FINE_LOCATION",
+        "ACCESS_COARSE_LOCATION"
+      ]
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    extra: {
+      eas: {
+        projectId: "0e8bb595-d919-426b-ba18-6d6717627795"
+      }
+    }
+  }
+};
